8cd4c45a6e7728540d7dfa9c51863be3a4eeeb15
Javad Miraftabzadeh <javad.miraftabzadeh@gmail.com>
2022-12-02 13:20:46 +0300

    utilities add PolynomialRegression function

--- utilities.py
+++ utilities.py
@@ -1,6 +1,9 @@
 import seaborn as sns
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+from sklearn.pipeline import Pipeline, make_pipeline
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
 
 # Plot function for results
 def DistributionPlot(redFunc, blueFunc, redName, blueName, title):
@@ -20,3 +23,7 @@ def DistributionPlot(redFunc, blueFunc, redName, blueName, title):
     plt.legend()
 
     plt.show()
+
+# create polynomial linear regression pipline
+def PolynomialRegression(degree=2, **kwargs):
+    return make_pipeline(PolynomialFeatures(degree), LinearRegression(**kwargs))
